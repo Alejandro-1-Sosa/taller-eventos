7504822acf6e99470bf4943c5965a144ce957305
Alejandro Sosa <sosaa9302@gmail.com>
2023-08-10 21:31:54 -0300

    Sitio web creado

--- eventos.js
+++ eventos.js
@@ -2,7 +2,6 @@ let element = document.getElementById("sitio");
 button = document.getElementById("boton");
 element.addEventListener('click',myfunction);
  function myfunction(){
-    myfunction.stopPropagation();
     alert("Hola!Soy el div"); 
  }
 
